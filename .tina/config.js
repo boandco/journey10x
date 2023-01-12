be114feb0925387145cdc5a89f1ea84d532922be
Connell McCarthy <connellmccarthy@gmail.com>
2023-01-12 10:28:47 -0500

    Update CMS from Forestry to Tina

---
+++ .tina/config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "5325351b-69b8-417d-a56c-1a18d8760fae", // Get this from tina.io
+  token: "51c9738fbf178135105f347073122c83ad48f7a5", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        label: "Projects",
+        name: "projects",
+        path: "_projects",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
